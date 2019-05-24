44dbb3e7485e84a900362544f5022d781b5194ed
eg <y.ge1222@gmail.com>
2019-05-24 13:15:31 +0200

    add connaire mechanism

--- src/dataGen.py
+++ src/dataGen.py
@@ -25,8 +25,8 @@ class ReactorOde:
         rho = self.gas.density
 
         wdot = self.gas.net_production_rates
-        dTdt = - (np.dot(self.gas.partial_molar_enthalpies, wdot) /
-                  (rho * self.gas.cp))
+        dTdt = -(np.dot(self.gas.partial_molar_enthalpies, wdot) /
+                 (rho * self.gas.cp))
         dYdt = wdot * self.gas.molecular_weights / rho
 
         return np.hstack((dTdt, dYdt))
@@ -48,7 +48,9 @@ def ignite_f(ini):
     for dt in dt_dict:
         if fuel == 'H2':
             # gas = ct.Solution('./data/Boivin_newTherm.cti')
-            gas = ct.Solution('../data/h2_sandiego.cti')
+            # gas = ct.Solution('../data/h2_sandiego.cti')
+            gas = ct.Solution('../data/connaire.cti')
+
         if fuel == 'CH4':
             gas = ct.Solution('../data/grimech12.cti')
             # gas = ct.Solution('gri30.xml')
@@ -67,23 +69,28 @@ def ignite_f(ini):
                 dt_ini = np.random.random_sample() * dt_base
                 solver.integrate(solver.t + dt_ini)
 
-            dt = dt_base * (0.9+round(0.2*np.random.random(), 2))
-            state_org = np.hstack(
-                [gas[gas.species_names].concentrations, np.dot(gas.partial_molar_enthalpies, gas[gas.species_names].X),
-                 gas.T, gas.density, gas.cp, dt, n_fuel])
+            dt = dt_base * (0.9 + round(0.2 * np.random.random(), 2))
+            state_org = np.hstack([
+                gas[gas.species_names].concentrations,
+                np.dot(gas.partial_molar_enthalpies, gas[gas.species_names].X),
+                gas.T, gas.density, gas.cp, dt, n_fuel
+            ])
 
             solver.integrate(solver.t + dt)
 
             gas.TPY = solver.y[0], P, solver.y[1:]
 
             # Extract the state of the reactor
-            state_new = np.hstack(
-                [gas[gas.species_names].concentrations, np.dot(gas.partial_molar_enthalpies, gas[gas.species_names].X),
-                 gas.T, gas.density, gas.cp, dt, n_fuel])
+            state_new = np.hstack([
+                gas[gas.species_names].concentrations,
+                np.dot(gas.partial_molar_enthalpies, gas[gas.species_names].X),
+                gas.T, gas.density, gas.cp, dt, n_fuel
+            ])
 
             # state_new = np.hstack([gas[gas.species_names].Y])
             state_res = state_new - state_org
-            res = abs(state_res[state_org != 0] / state_org[state_org != 0])/dt
+            res = abs(
+                state_res[state_org != 0] / state_org[state_org != 0]) / dt
             # res[res==np.inf]=0
             # res = np.nan_to_num(res)
             # res=res[res!=0]
@@ -94,11 +101,13 @@ def ignite_f(ini):
             # train_new.append(state_new)
 
             # if (abs(state_res.max() / state_org.max()) < 1e-5 and (solver.t / dt) > 200):
-            if ((res.max() < 1e3 and (solver.t / dt) > 50)) or (gas['H2'].X < 0.005 or gas['H2'].X > 0.995):
+            if ((res.max() < 1e3 and
+                 (solver.t / dt) > 50)) or (gas['H2'].X < 0.005
+                                            or gas['H2'].X > 0.995):
                 # if res.max() < 1e-5:
                 break
-        train_old = tmp[:len(tmp)-2]
-        train_org = tmp[1:len(tmp)-1]
+        train_old = tmp[:len(tmp) - 2]
+        train_org = tmp[1:len(tmp) - 1]
         train_new = tmp[2:len(tmp)]
 
     return train_old, train_org, train_new
@@ -106,7 +115,7 @@ def ignite_f(ini):
 
 def dataGeneration():
 
-    T = np.random.rand(20) * 1200 + 1001
+    T = np.random.rand(2) * 1200 + 1001
 
     # n_s = np.random.rand(30) * 30 + 0.1
     # n_l = np.random.rand(30) * 30
@@ -129,13 +138,14 @@ def dataGeneration():
 
     print('a[0][0]={}'.format(len(a[0][0])))
     e = time.time()
-    print('There are {} sets.'.format(len(a)), e-s)
+    print('There are {} sets.'.format(len(a)), e - s)
 
     old = np.concatenate([x[0] for x in a])
     org = np.concatenate([x[1] for x in a])
     new = np.concatenate([x[2] for x in a])
 
-    gas = ct.Solution('../data/h2_sandiego.cti')
+    # gas = ct.Solution('../data/h2_sandiego.cti')
+    gas = ct.Solution('../data/connaire.cti')
     # gas = ct.Solution('../data/grimech12.cti')
     columnNames = gas.species_names
     columnNames = columnNames + ['Hs']
@@ -159,7 +169,7 @@ def dataGeneration():
     train_new.to_hdf('tmp.h5', key='new', format='table')
 
     e = time.time()
-    print('saving takes {}s'.format(e-s))
+    print('saving takes {}s'.format(e - s))
 
 
 def ignite_post(ini):
@@ -188,7 +198,7 @@ def ignite_post(ini):
         solver = scipy.integrate.ode(ode)
         solver.set_integrator('vode', method='bdf', with_jacobian=True)
         solver.set_initial_value(y0, 0.0)
-#         dt_base = dt
+        #         dt_base = dt
         while solver.successful() and solver.t < t_end:
 
             if solver.t == 0:
@@ -196,31 +206,38 @@ def ignite_post(ini):
                 dt_ini = dt
                 solver.integrate(solver.t + dt_ini)
 
+
 #             dt = dt_base * (0.9+round(0.2*np.random.random(),2))
-            state_org = np.hstack(
-                [gas[gas.species_names].concentrations, np.dot(gas.partial_molar_enthalpies, gas[gas.species_names].X),
-                 gas.T, gas.density, gas.cp, dt, n_fuel])
+            state_org = np.hstack([
+                gas[gas.species_names].concentrations,
+                np.dot(gas.partial_molar_enthalpies, gas[gas.species_names].X),
+                gas.T, gas.density, gas.cp, dt, n_fuel
+            ])
 
             solver.integrate(solver.t + dt)
 
             gas.TPY = solver.y[0], P, solver.y[1:]
 
             # Extract the state of the reactor
-            state_new = np.hstack(
-                [gas[gas.species_names].concentrations, np.dot(gas.partial_molar_enthalpies, gas[gas.species_names].X),
-                 gas.T, gas.density, gas.cp, dt, n_fuel])
+            state_new = np.hstack([
+                gas[gas.species_names].concentrations,
+                np.dot(gas.partial_molar_enthalpies, gas[gas.species_names].X),
+                gas.T, gas.density, gas.cp, dt, n_fuel
+            ])
 
             # state_new = np.hstack([gas[gas.species_names].Y])
             state_res = state_new - state_org
             res = abs(state_res[:-2][state_org[:-2] != 0] /
-                      state_org[:-2][state_org[:-2] != 0])/dt
+                      state_org[:-2][state_org[:-2] != 0]) / dt
 
             # Update the sample
             train_org.append(state_org)
             train_new.append(state_new)
 
             # if (abs(state_res.max() / state_org.max()) < 1e-5 and (solver.t / dt) > 200):
-            if ((res.max() < 1e3 and (solver.t / dt) > 50)) or (gas['H2'].X < 0.005 or gas['H2'].X > 0.995):
+            if ((res.max() < 1e3 and
+                 (solver.t / dt) > 50)) or (gas['H2'].X < 0.005
+                                            or gas['H2'].X > 0.995):
                 # if res.max() < 1e-5:
                 break
 
@@ -231,12 +248,10 @@ def test_data(temp, n_fuel, columns, dt):
     ode_o, ode_n = ignite_post((temp, n_fuel, 'H2', dt))
     ode_o = np.asarray(ode_o)
     ode_n = np.asarray(ode_n)
-#     ode_o = np.append(ode_o, n_fuel * np.ones((ode_o.shape[0], 1)), axis=1)
-#     ode_n = np.append(ode_n, n_fuel * np.ones((ode_n.shape[0], 1)), axis=1)
-    ode_o = pd.DataFrame(data=ode_o,
-                         columns=columns)
-    ode_n = pd.DataFrame(data=ode_n,
-                         columns=columns)
+    #     ode_o = np.append(ode_o, n_fuel * np.ones((ode_o.shape[0], 1)), axis=1)
+    #     ode_n = np.append(ode_n, n_fuel * np.ones((ode_n.shape[0], 1)), axis=1)
+    ode_o = pd.DataFrame(data=ode_o, columns=columns)
+    ode_n = pd.DataFrame(data=ode_n, columns=columns)
 
     idx_test = (ode_o > 0).all(1)
     ode_o = ode_o[idx_test]
