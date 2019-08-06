586c1a988922ae4312eb8a44bdbd385b2828ec05
edison <y.ge1222@gmail.com>
2019-08-07 00:34:38 +0200

    parallel flamelet

--- src/flamelet.py
+++ src/flamelet.py
@@ -1,10 +1,6 @@
 #%%
-# This file is part of Cantera. See License.txt in the top-level directory or
-# at http://www.cantera.org/license.txt for license and copyright information.
 """
-This example creates two batches of counterflow diffusion flame simulations.
-The first batch computes counterflow flames at increasing pressure, the second
-at increasing strain rates.
+This example creates batches of counterflow diffusion flame simulations at increasing strain rates.
 
 The tutorial makes use of the scaling rules derived by Fiala and Sattelmayer
 (doi:10.1155/2014/484372). Please refer to this publication for a detailed
@@ -13,8 +9,8 @@ explanation. Also, please don't forget to cite it if you make use of it.
 This example can, for example, be used to iterate to a counterflow diffusion flame to an
 awkward  pressure and strain rate, or to create the basis for a flamelet table.
 """
-
 import copy
+import multiprocessing as mp
 import os
 import shutil
 
@@ -87,7 +83,6 @@ def Hs_T_rates(f):
 
 #%% PART 1: INITIALIZATION
 
-# reaction_mechanism = 'h2o2.xml'
 # reaction_mechanism = 'gri30.xml'
 reaction_mechanism = './data/gri12/grimech12.cti'
 gas = ct.Solution(reaction_mechanism)
@@ -96,7 +91,7 @@ grid_ini = width * np.linspace(0, 1, 20)
 # f = ct.CounterflowDiffusionFlame(gas, width=width)
 
 f = ct.CounterflowDiffusionFlame(gas, grid=grid_ini)
-f.set_max_grid_points(f.flame, 10000)
+f.set_max_grid_points(f.flame, 20000)
 
 # Define the operating pressure and boundary conditions
 f.P = ct.one_atm  # 1 bar
@@ -112,8 +107,8 @@ f.oxidizer_inlet.Y = 'O2:0.23,N2:0.77'
 f.oxidizer_inlet.T = 300  # K
 
 # Set refinement parameters, if used
-# f.set_refine_criteria(ratio=100.0, slope=0.002, curve=0.005, prune=0.0001)
-f.set_refine_criteria(ratio=100.0, slope=0.2, curve=0.5, prune=0.0001)
+f.set_refine_criteria(ratio=100.0, slope=0.001, curve=0.002, prune=0.0001)
+# f.set_refine_criteria(ratio=100.0, slope=0.2, curve=0.5, prune=0.0001)
 
 f.set_interrupt(interrupt_extinction)
 
@@ -128,73 +123,77 @@ f.save(data_directory + file_name,
        description='Cantera version ' + ct.__version__ +
        ', reaction mechanism ' + reaction_mechanism)
 
+
 #%% STRAIN RATE LOOP
+def flamelet_gen(i):
+    # Compute counterflow diffusion flames at increasing strain rates at 1 bar
+    # The strain rate is assumed to increase by 25% in each step until the flame is
+    # extinguished
+    strain_factor = 1.02**i
 
-# Compute counterflow diffusion flames at increasing strain rates at 1 bar
-# The strain rate is assumed to increase by 25% in each step until the flame is
-# extinguished
-# strain_factor = 1.0201
-strain_factor = 1.21
+    exp_d_a = -1. / 2.
+    exp_u_a = 1. / 2.
+    exp_V_a = 1.
+    exp_lam_a = 2.
+    exp_mdot_a = 1. / 2.
 
-# Exponents for the initial solution variation with changes in strain rate
-# Taken from Fiala and Sattelmayer (2014)
-exp_d_a = -1. / 2.
-exp_u_a = 1. / 2.
-exp_V_a = 1.
-exp_lam_a = 2.
-exp_mdot_a = 1. / 2.
+    # Restore initial solution
+    file_name = 'initial_solution.xml'
+    f.restore(filename=data_directory + file_name, name='solution', loglevel=0)
 
-# Restore initial solution
-file_name = 'initial_solution.xml'
-f.restore(filename=data_directory + file_name, name='solution', loglevel=0)
+    # Do the strain rate loop
+    if np.max(f.T) > temperature_limit_extinction:
+        print('strain rate iteration', i)
+        # Create an initial guess based on the previous solution
+        # Update grid
+        f.flame.grid *= strain_factor**exp_d_a
+        normalized_grid = f.grid / (f.grid[-1] - f.grid[0])
+        # Update mass fluxes
+        f.fuel_inlet.mdot *= strain_factor**exp_mdot_a
+        f.oxidizer_inlet.mdot *= strain_factor**exp_mdot_a
+        # Update velocities
+        f.set_profile('u', normalized_grid, f.u * strain_factor**exp_u_a)
+        f.set_profile('V', normalized_grid, f.V * strain_factor**exp_V_a)
+        # Update pressure curvature
+        f.set_profile('lambda', normalized_grid,
+                      f.L * strain_factor**exp_lam_a)
+        try:
+            # Try solving the flame
+            f.solve(loglevel=0, refine_grid=True)
+            file_name = 'strain_loop_' + format(i, '02d') + '.xml'
+            f.save(data_directory + file_name,
+                   name='solution',
+                   loglevel=1,
+                   description='Cantera version ' + ct.__version__ +
+                   ', reaction mechanism ' + reaction_mechanism)
+        except FlameExtinguished:
+            print('Flame extinguished')
 
-# Counter to identify the loop
-n = 0
-# Do the strain rate loop
-while np.max(f.T) > temperature_limit_extinction:
-    n += 1
-    print('strain rate iteration', n)
-    # Create an initial guess based on the previous solution
-    # Update grid
-    f.flame.grid *= strain_factor**exp_d_a
-    normalized_grid = f.grid / (f.grid[-1] - f.grid[0])
-    # Update mass fluxes
-    f.fuel_inlet.mdot *= strain_factor**exp_mdot_a
-    f.oxidizer_inlet.mdot *= strain_factor**exp_mdot_a
-    # Update velocities
-    f.set_profile('u', normalized_grid, f.u * strain_factor**exp_u_a)
-    f.set_profile('V', normalized_grid, f.V * strain_factor**exp_V_a)
-    # Update pressure curvature
-    f.set_profile('lambda', normalized_grid, f.L * strain_factor**exp_lam_a)
-    try:
-        # Try solving the flame
-        f.solve(loglevel=0, refine_grid=True)
-        file_name = 'strain_loop_' + format(n, '02d') + '.xml'
-        f.save(data_directory + file_name,
-               name='solution',
-               loglevel=1,
-               description='Cantera version ' + ct.__version__ +
-               ', reaction mechanism ' + reaction_mechanism)
-    except FlameExtinguished:
-        print('Flame extinguished')
-        break
-    except ct.CanteraError as e:
-        print('Error occurred while solving:', e)
-        break
+        except ct.CanteraError as e:
+            print('Error occurred while solving:', e)
 
-    print(f.strain_rate('max'))
-    if (n == 2):
-        break
+    strain_max = f.strain_rate('max')
+    print(strain_max)
 
+    return strain_max
+
+
+#%% parallel running
+
+p = mp.Pool(mp.cpu_count())
+flamelet_range = [x for x in range(192)]
+p.map(flamelet_gen, flamelet_range)
 #%%
+n = 172
+
 sp_names = f.gas.species_names
 col_names = sp_names + ['Hs'] + ['Temp'] + ['id'] + ['grid'] + ['amax']
 c = np.empty((0, len(col_names)), float)
 wdot = np.empty((0, len(col_names)), float)
 
-for i in range(n - 1):
+for i in range(n):
     print(i)
-    file_name = 'strain_loop_{0:02d}.xml'.format(i + 1)
+    file_name = 'strain_loop_{0:02d}.xml'.format(i)
     f.restore(filename=data_directory + file_name, name='solution', loglevel=0)
     a_max = f.strain_rate('max')
 
@@ -203,9 +202,9 @@ for i in range(n - 1):
     id_mat = np.ones((w_mat.shape[1], 1)) * i
     amax_mat = np.ones((w_mat.shape[1], 1)) * a_max
     Hs_w, T_w, Hs_c, grid = Hs_T_rates(f)
+    T_c = f.T.reshape(-1, 1)
 
-    tmp_c = np.hstack((c_mat.T, Hs_c, f.T.reshape(-1,
-                                                  1), id_mat, grid, amax_mat))
+    tmp_c = np.hstack((c_mat.T, Hs_c, T_c, id_mat, grid, amax_mat))
     c = np.vstack((c, tmp_c))
 
     tmp_w = np.hstack((w_mat.T, Hs_w, T_w, id_mat, grid, amax_mat))
@@ -214,14 +213,16 @@ for i in range(n - 1):
 df_wdot = pd.DataFrame(wdot, columns=col_names)
 df_c = pd.DataFrame(c, columns=col_names)
 
+df_wdot['N2'] = 0
+
 df_c.to_hdf('CH4_flt.h5', key='c', format='table')
 df_wdot.to_hdf('CH4_flt.h5', key='wdot', format='table')
 
 #%%
-px.scatter_3d(df_c.sample(frac=1),
+px.scatter_3d(df_wdot.sample(frac=1),
               x='grid',
               y='id',
-              z='Temp',
+              z='Hs',
               title='concentration')
 # #%%
 # px.scatter_3d(df_wdot.sample(frac=0.001), x='grid', y='id', z='N2', title='rate')
